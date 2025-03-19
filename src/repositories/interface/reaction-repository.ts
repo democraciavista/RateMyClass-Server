7a228644d538ef9df220db9ac8d98a651e54879b
Ismael <henriqueisma999@gmail.com>
2025-03-19 19:19:10 -0300

    feat(reactions ): add repositories

--- src/repositories/interface/reaction-repository.ts
+++ src/repositories/interface/reaction-repository.ts
@@ -4,9 +4,13 @@ export interface IReactionRepository {
   create: (data: Prisma.ReactionUncheckedCreateInput) => Promise<Reaction>;
   findById: (id: string) => Promise<Reaction | null>;
   delete: (id: string) => Promise<Reaction>;
-  update: (
-    id: string,
-    data: Prisma.ReactionUpdateInput,
-  ) => Promise<Reaction>;
+  update: (id: string, data: Prisma.ReactionUpdateInput) => Promise<Reaction>;
   findAll: () => Promise<Reaction[]>;
+  findByFiltres: (data: {
+    type?: $Enums.ReactionType;
+    userId?: string;
+
+    materialId?: string;
+    disciplineId?: string;
+  }) => Promise<Reaction[]>;
 }
---
+++ src/repositories/prisma/prisma-reaction-repository.ts
@@ -0,0 +1,49 @@
+import prisma from '@database';
+import { $Enums, Prisma, Reaction } from '@prisma/client';
+import { IReactionRepository } from '@repositories/interface/reaction-repository';
+
+export class PrismaReactionRepository implements IReactionRepository {
+  async create(data: Prisma.ReactionUncheckedCreateInput) {
+    const reaction = await prisma.reaction.create({ data });
+    return reaction;
+  }
+
+  async findById(id: string) {
+    const reaction = await prisma.reaction.findUnique({ where: { id } });
+    return reaction;
+  }
+
+  async delete(id: string) {
+    const reaction = await prisma.reaction.delete({ where: { id } });
+    return reaction;
+  }
+
+  async update(id: string, data: Prisma.ReactionUpdateInput) {
+    const reaction = await prisma.reaction.update({ where: { id }, data });
+
+    return reaction;
+  }
+
+  async findAll() {
+    const reactions = await prisma.reaction.findMany();
+    return reactions;
+  }
+  async findByFiltres(data: {
+    type?: $Enums.ReactionType;
+    userId?: string;
+    materialId?: string;
+    disciplineId?: string;
+  }) {
+    const { type, userId, materialId, disciplineId } = data;
+    const reactions = await prisma.reaction.findMany({
+      where: {
+        type: type ? { equals: type } : undefined,
+        userId: userId ? { equals: userId } : undefined,
+        materialId: materialId ? { equals: materialId } : undefined,
+        disciplineId: disciplineId ? { equals: disciplineId } : undefined,
+      },
+    });
+
+    return reactions;
+  }
+}
