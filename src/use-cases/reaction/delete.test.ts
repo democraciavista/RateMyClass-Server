f42833571d915d1e945116cd4e9383bd7faa5994
Ismael <henriqueisma999@gmail.com>
2025-03-19 23:52:11 -0300

    feat(reaction): add delete  use case with tests

---
+++ src/use-cases/reaction/delete.test.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { InMemoryReactionRepository } from '@repositories/in-memory/in-memory-reaction-repository';
+import { DeleteReactionUseCase } from './delete';
+import { NotFoundError } from '@errors/not-found-error';
+import { $Enums, Reaction } from '@prisma/client';
+
+let reactionRepository: InMemoryReactionRepository;
+let sut: DeleteReactionUseCase;
+
+describe('RemoveReaction Use Case', () => {
+  beforeEach(() => {
+    reactionRepository = new InMemoryReactionRepository();
+    sut = new DeleteReactionUseCase(reactionRepository);
+  });
+
+  it('should delete a reaction when it exists', async () => {
+    const reactionData: Reaction = {
+      id: 'reaction-123',
+      userId: 'user-xyz',
+      materialId: 'material-abc',
+      disciplineId: null,
+      type: $Enums.ReactionType.LIKE,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    await reactionRepository.create(reactionData);
+
+    const { Reaction } = await sut.execute(reactionData.id);
+
+    expect(Reaction).toEqual(reactionData);
+
+    const deletedReaction = await reactionRepository.findById(reactionData.id);
+    expect(deletedReaction).toBeNull();
+  });
+
+  it('should throw NotFoundError if the reaction does not exist', async () => {
+    await expect(() => sut.execute('non-existent-id')).rejects.toBeInstanceOf(
+      NotFoundError,
+    );
+  });
+});
---
+++ src/use-cases/reaction/delete.ts
@@ -0,0 +1,21 @@
+import { NotFoundError } from '@errors/not-found-error';
+import { Reaction } from '@prisma/client';
+import { IReactionRepository } from '@repositories/interface/reaction-repository';
+
+interface RegisterReactionUseCaseResponse {
+  Reaction: Reaction;
+}
+
+export class DeleteReactionUseCase {
+  constructor(private ReactionRepository: IReactionRepository) {}
+
+  async execute(id: string): Promise<RegisterReactionUseCaseResponse> {
+    const ReactionExist = await this.ReactionRepository.findById(id);
+    if (!ReactionExist) {
+      throw new NotFoundError('Reação não encontrada');
+    }
+    const Reaction = await this.ReactionRepository.delete(id);
+
+    return { Reaction };
+  }
+}
